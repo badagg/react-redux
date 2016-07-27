d984989f60a573792a743f51e8db6bc80b7667d9
Z.Key <zkey@zhoubingdeMacBook-Pro.local>
2016-07-27 10:29:09 +0800

    add middlevare

--- src/js/app.js
+++ src/js/app.js
@@ -4,17 +4,14 @@ import '../css/app.scss';
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 
 import reducers from './redux/reducers';
 //import * as reducers from './redux/reducers';
 
-// const createStoreWithThunk = applyMiddleware(thunk)(createStore);
-// const reducer = combineReducers(reducers);
-// const store = createStoreWithThunk(reducer);
-
-const store = createStore(reducers)
+const createStoreWithThunk = applyMiddleware(thunk)(createStore);
+const store = createStoreWithThunk(reducers);
 
 // import component
 import App from './component/App';
